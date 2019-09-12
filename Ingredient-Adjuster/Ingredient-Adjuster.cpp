8df36845e9f05932f5a4503b66b45f5b22c90f70
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-12 13:41:36 -0600

    add basic calculations for adjusting the recipe based on num of cookies to bake

--- Ingredient-Adjuster/Ingredient-Adjuster.cpp
+++ Ingredient-Adjuster/Ingredient-Adjuster.cpp
@@ -4,9 +4,26 @@
 // 9/12/19
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-   cout << "Hello World!\n";
+	cout << fixed << setprecision(2);
+	float sugarNeeded = 1.5 / 48;
+	float butterNeeded = 1.0 / 48;
+	float flourNeeded = 2.75 / 48;
+
+	int numOfCookies;
+	cout << "Enter the number of cookies that you would like to bake: ";
+	cin >> numOfCookies;
+
+	float sugar = numOfCookies * sugarNeeded;
+	float butter = numOfCookies * butterNeeded;
+	float flour = numOfCookies * flourNeeded;
+
+	cout << "To make " << numOfCookies << " cookies you will need: \n";
+	cout << sugar << " cups of sugar.\n";
+	cout << butter << " cups of butter.\n";
+	cout << flour << " cups of flour.\n";
 }
